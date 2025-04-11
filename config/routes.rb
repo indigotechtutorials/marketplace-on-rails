0ed9e80dc419c4b833f603ade7275f64c9b137f1
reevotek <reevotek@outlook.com>
2025-04-11 09:18:58 -0700

    Adds new landing page, collections/categories section and footer

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "pages#home"
+  # root "pages#home"
+  root "pages#landing"
 end
